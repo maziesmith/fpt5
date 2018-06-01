59c4da8fe541ca4db72d9d41d17b371b21d80fb3
nguyenhongphat0 <hongphat.js@gmail.com>
2018-06-01 21:41:47 +0700

    add mono project

---
+++ mono/TestingGTK/TestingGTK/MainWindow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow: Gtk.Window
+{
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected void OnHelloBtnClicked (object sender, EventArgs e)
+	{
+		new MessageDialog (this, 0, MessageType.Info, ButtonsType.Close, "Hello", "Message")
+			.Show();
+	}
+}
