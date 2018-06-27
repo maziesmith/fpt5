10f0a15105b31eaa72e1b3056288675e03a30883
nguyenhongphat0 <nguyenhongphat28121998@gmail.com>
2018-06-27 08:44:59 +0700

    add lab1 webform

---
+++ TestingNUnit/TestingNUnit/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TestingNUnit
+{
+    class Program
+    {
+        public int DayInMonth(int month, int year)
+        {
+            if (month < 1 || month > 12 || year < 1000 || year > 3000)
+            {
+                return 0;
+            }
+            if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+            {
+                return 31;
+            }
+            if (month == 4 || month == 6 || month == 9 || month == 11)
+            {
+                return 30;
+            }
+            if (DateTime.IsLeapYear(year))
+            {
+                return 29;
+            } else
+            {
+                return 28;
+            }
+        }
+
+        public bool IsValidDate(int day, int month, int year)
+        {
+            if (day < 1 || month < 1 || month > 12 || year < 1000 || year > 3000)
+            {
+                return false;
+            }
+            int dim = DayInMonth(month, year);
+            if (day > dim)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ TestingNUnit/TestingNUnit/Test.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingNUnit
+{
+    [TestFixture]
+    class Test
+    {
+        [Test]
+        public void CheckDayInMonth()
+        {
+
+            Program p = new Program();
+            Assert.AreEqual(31, p.DayInMonth(1, 2000));
+            Assert.AreEqual(0, p.DayInMonth(2, 999));
+            Assert.AreEqual(31, p.DayInMonth(3, 2031));
+            Assert.AreEqual(31, p.DayInMonth(2, 2000));
+            Assert.AreEqual(31, p.DayInMonth(0, 2000));
+            Assert.AreEqual(29, p.DayInMonth(12, 2000));
+            Assert.AreEqual(29, p.DayInMonth(2, 2016));
+            Assert.AreEqual(29, p.DayInMonth(2, 2008));
+            Assert.AreEqual(29, p.DayInMonth(2, 2012));
+            Assert.AreEqual(29, p.DayInMonth(2, 3000));
+        }
+
+        [Test]
+        public void CheckIsValidDate()
+        {
+            Program p = new Program();
+            Assert.AreEqual(true, p.IsValidDate(10, 0, 1000));
+            Assert.AreEqual(false, p.IsValidDate(28, 12, 2000));
+            Assert.AreEqual(true, p.IsValidDate(31, 12, 2012));
+            Assert.AreEqual(false, p.IsValidDate(29, 2, 2002));
+            Assert.AreEqual(true, p.IsValidDate(32, 1, 2010));
+            Assert.AreEqual(false, p.IsValidDate(29, 2, 2010));
+            Assert.AreEqual(false, p.IsValidDate(29, 2, 2011));
+            Assert.AreEqual(true, p.IsValidDate(29, 2, 2011));
+            Assert.AreEqual(false, p.IsValidDate(29, 2, 2016));
+            Assert.AreEqual(true, p.IsValidDate(28, 2, 2018));
+            Assert.AreEqual(false, p.IsValidDate(29, 2, 2000));
+        }
+    }
+}
